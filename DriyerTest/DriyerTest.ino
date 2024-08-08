5b951d7fc1b59dd9f1e3f546c4b3e75aab7ab08c
alireza <jonasdark086@gmail.com>
2024-08-08 11:52:59 +0330

    fix

--- DriyerTest/DriyerTest.ino
+++ DriyerTest/DriyerTest.ino
@@ -253,7 +253,7 @@ void run_program() {
   while (true) { delay(0);
     
     counter++;
-    if(counter > 50) {
+    if(counter > 500) {
       Serial.println("Loop!");
       counter = 0;
       lcd.init();
